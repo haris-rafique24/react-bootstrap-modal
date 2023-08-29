46c40890f632c9d1a34d0f8e484e1cdbc286d203
haris-rafique24 <62500642+haris-rafique24@users.noreply.github.com>
2023-08-29 10:22:57 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import ModalComponent from './ModalComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ModalComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ModalComponent.js
@@ -0,0 +1,37 @@
+// src/ModalComponent.js
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+const ModalComponent = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Open Modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal Title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This is the modal content. You can put any JSX here.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalComponent;
